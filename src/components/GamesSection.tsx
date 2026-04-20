1f0892883c7d91dedf12a7117e6e610558738755
Clayton-Modder <cmempresa.ru@proton.me>
2026-04-19 22:35:42 -0300

    Update GamesSection.tsx

--- src/components/GamesSection.tsx
+++ src/components/GamesSection.tsx
@@ -49,7 +49,7 @@ const GamesSection = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch("https://embedtv.cv/jogos.php")
+    fetch("https://embedtv.live/jogos.php")
       .then((res) => res.json())
       .then((data) => {
         setGames(Array.isArray(data) ? data : []);
